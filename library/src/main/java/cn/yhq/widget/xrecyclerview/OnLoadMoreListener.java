1d759d17fadc0a388fb62dcb40f1a7aae514f312
yanghuiqiang <694551594@qq.com>
2017-02-08 09:56:03 +0800

    初始导入。

---
+++ library/src/main/java/cn/yhq/widget/xrecyclerview/OnLoadMoreListener.java
@@ -0,0 +1,10 @@
+package cn.yhq.widget.xrecyclerview;
+
+/**
+ * Created by aspsine on 16/3/13.
+ */
+public interface OnLoadMoreListener {
+
+    void onLoadMore();
+
+}
---
+++ library/src/main/java/cn/yhq/widget/xrecyclerview/OnRefreshListener.java
@@ -0,0 +1,8 @@
+package cn.yhq.widget.xrecyclerview;
+
+/**
+ * Created by aspsine on 16/3/13.
+ */
+public interface OnRefreshListener {
+    void onRefresh();
+}
